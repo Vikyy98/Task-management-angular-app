8c7bd329d7ddf63818a44e9294c9caaf6a923c7d
Vikyy98 <Vigneshwaran.kumaravel@marlensoft.com>
2025-07-25 08:27:08 +0530

    Testing push

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { AppComponent } from '../app.component';
-import { TaskComponent } from "./task/task.component";
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [AppComponent, TaskComponent],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
